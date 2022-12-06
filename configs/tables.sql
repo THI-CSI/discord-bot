96251ab46f862303b0e05f6cb403c59290a789ac
Berkan E <60660058+bee1850@users.noreply.github.com>
2022-12-06 12:52:37 +0100

    Update tables.sql

--- configs/tables.sql
+++ configs/tables.sql
@@ -1,5 +1,12 @@
+CREATE TABLE bot.roles (
+  `roleId` VARCHAR(20) PRIMARY KEY NOT NULL,
+  `serverId` VARCHAR(20) NOT NULL,
+  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `comment` VARCHAR(255) DEFAULT NULL
+) ENGINE=INNODB;
+
 CREATE TABLE bot.tokens (
-	`token` VARCHAR(6) NOT NULL,
+	`token` VARCHAR(6) NOT NULL DEFAULT UUID(),
 	`targetRole` VARCHAR(20) NOT NULL,
 	`comment` TEXT NULL DEFAULT NULL,
 	`createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -8,32 +15,36 @@ CREATE TABLE bot.tokens (
 	FOREIGN KEY (`targetRole`) REFERENCES `roles` (`roleId`)
 ) ENGINE = INNODB;
 
-CREATE TABLE bot.roles (
-  `roleId` VARCHAR(20) PRIMARY KEY NOT NULL,
-  `serverId` VARCHAR(20) NOT NULL,
-  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  `comment` VARCHAR(255) DEFAULT NULL
-) ENGINE=INNODB;
-
-CREATE TABLE bot.role_permissions (
-  `permissionId` VARCHAR(20) PRIMARY KEY NOT NULL,
-  `roleId` VARCHAR(20) NOT NULL,
-  `permission` VARCHAR(255) NOT NULL,
-  `comment` VARCHAR(255) DEFAULT NULL,
-  FOREIGN KEY (`roleId`) REFERENCES `roles`(`roleId`)
-) ENGINE=INNODB;
-
 /*
 CREATE TABLE bot.users (
   `userId` VARCHAR(20) PRIMARY KEY,
   `data` LONGBLOB
 );
+*/
 
+CREATE TABLE bot.role_permissions (
+  `permissionId` VARCHAR(50) DEFAULT UUID(),
+  `roleId` VARCHAR(20) NOT NULL,
+  `permission` VARCHAR(255) NOT NULL,
+  `comment` VARCHAR(255) DEFAULT NULL,
+  `grantedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grantedBy` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`permissionId`),
+  FOREIGN KEY (`roleId`) REFERENCES `roles`(`roleId`),
+  FOREIGN KEY (`grantedBy`) REFERENCES `users`(`userId`)
+) ENGINE=INNODB;
+
+/*
 CREATE TABLE bot.user_permissions (
-  `permissionId` VARCHAR(20) PRIMARY KEY NOT NULL,
+  `permissionId` VARCHAR(50) DEFAULT UUID(),
   `userId` VARCHAR(20) NOT NULL,
   `permission` VARCHAR(255) NOT NULL,
   `comment` VARCHAR(255) DEFAULT NULL,
-  FOREIGN KEY (`userId`) REFERENCES `users`(`userId`)
+  `grantedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grantedBy` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`permissionId`),
+  FOREIGN KEY (`userId`) REFERENCES `users`(`userId`),
+  FOREIGN KEY (`grantedBy`) REFERENCES `users`(`userId`)
 ) ENGINE=INNODB;
+
 */
