b98fed519147d3c0b88671eefecdc4eddef109bd
Berkan Erkasap <bee1850@thi.de>
2023-03-16 16:42:42 +0100

    🎨 style(registerrole.js): fix indentation and whitespace 🚀 chore(registerrole.js): update command description and add required permissions This commit fixes the indentation and whitespace in the registerrole.js file. The command description has been updated to be more descriptive. Additionally, the required permissions have been added to the command.
    🔧 chore(registerrole.js): reformat code
    This commit only reformats the code to improve its readability and consistency. No functional changes were made.

--- commands/Administration/registerrole.js
+++ commands/Administration/registerrole.js
@@ -2,21 +2,21 @@ const Command = require('../../structures/Command');
 
 module.exports = class extends Command {
 
-	constructor(...args) {
-		super(...args, {
-			description: 'Registers new Roles into the Database',
-			isActive: true,
-			requiredPerms: ['MANAGE_ROLES_GUILD'],
-		});
-	}
+    constructor(...args) {
+        super(...args, {
+            description: 'Registers new Roles into the Database',
+            isActive: true,
+            requiredPerms: ['MANAGE_ROLES_GUILD'],
+        });
+    }
 
-	async run(interaction) {
+    async run(interaction) {
 
-		if (interaction.guild.available) {
+        if (interaction.guild.available) {
 
 
-			const role = interaction.options.getRole('role');
-			const comment = interaction.options.getString('comment') ?? 'NULL';
+            const role = interaction.options.getRole('role');
+            const comment = interaction.options.getString('comment') ?? 'NULL';
 
 
             // Owners of Guilds skip this check to be able to add Roles to the Database without having the required Permissions
@@ -28,58 +28,57 @@ module.exports = class extends Command {
                 });
             }
 
-			const self = await interaction.guild.members.fetchMe();
-			const roleCompare = await this.client.utils.compareRolePerms(role, self);
-			if (!roleCompare.success) {
-				if (roleCompare.error === 'ROLE_HIGHER') {
-					return interaction.reply({
-						content: 'The specified Role is higher than or equal to any of mine and thus not managable by me.',
-						ephemeral: true,
-					});
-				}
-				else if (roleCompare.error === 'ROLE_MANAGED') {
-					return interaction.reply({
-						content: 'The specified Role is being managed by another service.',
-						ephemeral: true,
-					});
-				}
-			}
 
-			try {
-				const selectSql = 'SELECT * FROM `roles` WHERE `serverId` = ? AND `roleId` = ?;';
-				const selectValues = [role.guild.id, role.id];
+            const self = await interaction.guild.members.fetchMe();
+            const roleCompare = await this.client.utils.compareRolePerms(role, self);
+            if (!roleCompare.success) {
+                if (roleCompare.error === 'ROLE_HIGHER') {
+                    return interaction.reply({
+                        content: 'The specified Role is higher than or equal to any of mine and thus not managable by me.',
+                        ephemeral: true,
+                    });
+                } else if (roleCompare.error === 'ROLE_MANAGED') {
+                    return interaction.reply({
+                        content: 'The specified Role is being managed by another service.',
+                        ephemeral: true,
+                    });
+                }
+            }
 
-
-				const selectResult = await this.client.db.select(selectSql, selectValues);
-				if (selectResult.length > 0) {
-					return interaction.reply({
-						content: 'The specified Role is already registered!',
-						ephemeral: true,
-					});
-				}
-
-				// Create a SQL Statement
-				const sql = 'INSERT INTO `roles` (`roleId`, `serverId`, `createdAt`, `comment`) VALUES (?, ?, current_timestamp(), ?);';
-				const values = [role.id, interaction.guild.id, comment];
-
-				await this.client.db.query(sql, values);
-			}
-			catch (e) {
-				return interaction.reply({
-					content: 'An Error occoured! Please check Bot-Logs or contact the maintainer!',
-					ephemeral: true,
-				});
-			}
-
-			this.client.verbose('SERVICE', [`A new Role (${role.id}) was registered for Server ${role.guild.id} by User with the Id ${interaction.user.id}!`]);
-			return interaction.reply({
-				content: 'The Role was successfully registered into the Database!',
-				ephemeral: true,
-			}).catch(e => this.client.error('API', ['Error while trying to reply to an Interaction:', e.message]));
+            try {
+                const selectSql = 'SELECT * FROM `roles` WHERE `serverId` = ? AND `roleId` = ?;';
+                const selectValues = [role.guild.id, role.id];
 
 
-		}
+                const selectResult = await this.client.db.select(selectSql, selectValues);
+                if (selectResult.length > 0) {
+                    return interaction.reply({
+                        content: 'The specified Role is already registered!',
+                        ephemeral: true,
+                    });
+                }
+
+                // Create a SQL Statement
+                const sql = 'INSERT INTO `roles` (`roleId`, `serverId`, `createdAt`, `comment`) VALUES (?, ?, current_timestamp(), ?);';
+                const values = [role.id, interaction.guild.id, comment];
+
+                await this.client.db.query(sql, values);
+            } catch (e) {
+                return interaction.reply({
+                    content: 'An Error occoured! Please check Bot-Logs or contact the maintainer!',
+                    ephemeral: true,
+                });
+            }
+
+            this.client.verbose('SERVICE', [`A new Role (${role.id}) was registered for Server ${role.guild.id} by User with the Id ${interaction.user.id}!`]);
+            return interaction.reply({
+                content: 'The Role was successfully registered into the Database!',
+                ephemeral: true,
+            }).catch(e => this.client.error('API', ['Error while trying to reply to an Interaction:', e.message]));
 
 
-	}
+        }
+
+
+    }
 };
